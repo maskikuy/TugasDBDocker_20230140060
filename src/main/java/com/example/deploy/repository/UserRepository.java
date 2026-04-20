af9c0c204ccffa93a7473b465a8d7a4711d12bbf
maskikuy <kproject305@gmail.com>
2026-04-20 14:14:18 +0700

    Menambahkan repostitory class untuk User

---
+++ src/main/java/com/example/deploy/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.deploy.repository;
+
+import com.example.deploy.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,String> {
+}
