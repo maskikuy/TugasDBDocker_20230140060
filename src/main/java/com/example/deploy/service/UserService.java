b2efdb774b4050ad5f66d760085f9c948bd46f36
maskikuy <kproject305@gmail.com>
2026-04-20 14:15:17 +0700

    Menambahkan service class untuk User

---
+++ src/main/java/com/example/deploy/service/UserService.java
@@ -0,0 +1,33 @@
+package com.example.deploy.service;
+
+import com.example.deploy.model.User;
+import com.example.deploy.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User addUser(User request) {
+        request.setId(UUID.randomUUID().toString());
+        return userRepository.save(request);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(String id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    }
+}
